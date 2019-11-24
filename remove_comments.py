467aedd73fef4254d051ff8f8d6b69638510edcc
Daktoa Powers <46329980+doogie205@users.noreply.github.com>
2019-11-24 12:11:08 -0500

    Update remove_comments.py
    Added Java support (with links)

--- remove_comments.py
+++ remove_comments.py
@@ -9,28 +9,35 @@ filename = input("What file would you like to remove comments from?\n")
 
 f = open(filename, "r")
 
-comment_type = input("What comments does your language use?\n")
+comment_type = input("What comments does your language use? (ex. //)\n")
 
 line = "{}Start".format(comment_type)
 cnt = 0
 whole_file = ""
 while line:
   if((re.search(comment_type, line) != None)):
-    temp_array = re.split(comment_type, line, 1)
-    #print(temp_array)
-    line = temp_array[0]
-    if(line == ""):
-      line = f.readline()
-      continue
+    if((comment_type == "//" and ((re.search("http://", line) == None) and ((re.search("https://", line) == None)))) or comment_type != "//"):
+      temp_array = re.split(comment_type, line, 1)
+      #print(temp_array)
+      line = temp_array[0]
+      if(line == ""):
+        line = f.readline()
+        continue
+      else:
+        whole_file += "\n{}".format(line.strip())
+        line = f.readline()
+        cnt += 1
     else:
-      whole_file += "\n{}".format(line.strip())
-      line = f.readline()
-      cnt += 1
+        whole_file += "\n{}".format(line.strip())
+        line = f.readline()
+        cnt += 1
   else:
    #print("Line {}: {}".format(cnt, line.strip()))
    whole_file += "\n{}".format(line.strip())
    line = f.readline()
    cnt += 1
+
+
    
   
    
@@ -44,3 +51,4 @@ f = open("{}".format(new_filename), "w")
 f.write(whole_file[1:len(whole_file)])
 
 f.close()
+
